890eaa50528c0b5f9ca45c4dc58e9e9eddfd0ed0
Lewi Samuel <arroba.lewi@gmail.com>
2022-10-08 01:45:19 -0300

    atualiza build

---
+++ built/app.d.ts
@@ -0,0 +1,28 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import { Context } from '@microsoft/mixed-reality-extension-sdk';
+/**
+ * The main class of this app. All the logic goes here.
+ */
+export default class HelloWorld {
+    private context;
+    private baseUrl;
+    private text;
+    private cube;
+    constructor(context: Context, baseUrl: string);
+    /**
+     * Once the context is "started", initialize the app.
+     */
+    private started;
+    /**
+     * Generate keyframe data for a simple spin animation.
+     * @param duration The length of time in seconds it takes to complete a full revolution.
+     * @param axis The axis of rotation in local space.
+     */
+    private generateSpinKeyframes;
+    private growAnimationData;
+    private shrinkAnimationData;
+}
+//# sourceMappingURL=app.d.ts.map
